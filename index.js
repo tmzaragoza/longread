f88ee5fa8c0da0c4bdae00bcc719d536d69b927d
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-01-13 11:21:25 -0800

    added handlers and working

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const server = require('./server');
 const router = require('./router');
+const requestHandlers = require('./requestHandlers')
+
+const handle = {}
+  handle['/'] = requestHandlers.start;
+  handle['/start'] = requestHandlers.start;
+  handle['/upload'] = requestHandlers.upload;
 
 
-server.start(router.route);
+server.start(router.route, handle);
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const http = require('http');
 const url = require('url');
 
-function start(route){
+function start(route, handle){
   function onRequest(request, response){
     const pathname = url.parse(request.url).pathname;
     console.log('Request for' + pathname + 'received.');
-    route(pathname);
+
+    route(handle, pathname);
+    
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('Hello World');
     response.end();
