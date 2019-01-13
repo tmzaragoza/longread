bc58dd741e71968717cec84eef1c4cb1fe5ac7a1
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-01-13 12:38:23 -0800

    can have text box

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const http = require('http');
 const url = require('url');
 
 function start(route, handle){
-  function onRequest(request, response){
+  function onRequest(request, response) {
+    const postData = '';
     const pathname = url.parse(request.url).pathname;
     console.log('Request for' + pathname + 'received.');
 
-    
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    const content = route(handle, pathname);
-    response.write(content);
-    response.end();
+    request.setEncoding('utf8');
+  
+    request.addListener('data', function(postDataChunk){
+      postData += postDataChunk;
+      console.log('"Received POST data chunk" + postDataChunk + "."');
+    });
+    request.addListener('end', function() {
+      route(handle, pathname, response, postData);
+    })
   }
   http.createServer(onRequest).listen(8888);
   console.log('Server has started');
